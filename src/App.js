28fe9188b4efe65cbcb42c56fd44fc3d01760cc9
Hippolyte EZIAN <polarusdev@gmail.com>
2022-05-30 18:44:36 +0100

    valid

--- src/App.js
+++ src/App.js
@@ -1,44 +1,66 @@
 import "./App.css";
 import { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
-import { SearchBox } from "./components/search-box/search-box.components";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       users: [],
-      searchField: "",
+      searchData: "",
+      cb: true,
     };
+    // console.log("Constructor");
   }
 
   componentDidMount() {
+    console.log("Component mounted");
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        return res.json();
-      })
-      .then((userList) => {
-        // console.log(userList);
-        this.setState({ users: userList });
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ users: json });
       });
   }
 
-  render() {
-    const { users, searchField } = this.state;
-    const filteredUsers = users.filter((user) => {
-      return user.name.toLowerCase().includes(searchField.toLowerCase());
-    });
+  componentDidUpdate() {
+    console.log("Component did update");
+  }
 
+  componentWillUnmount() {}
+
+  searchName = (event) => {
+    let value = event.target.value;
+    this.setState({ searchData: value });
+  };
+
+  render() {
+    const { users, searchData } = this.state;
     return (
       <div className="App">
-        <h1>Ramdom Users</h1>
-        <SearchBox
-          placeholder="Seach User"
-          handleChange={(event) =>
-            this.setState({ searchField: event.target.value })
-          }
+        <input
+          type="search"
+          placeholder="Search"
+          onChange={(event) => {
+            console.log(event);
+            // this.searchName();
+          }}
         />
-        <CardList users={filteredUsers} />
+
+        <input
+          type="checkbox"
+          placeholder="Search"
+          onChange={(event) => {
+            // console.log(event);
+            this.setState({ cb: event.target.checked });
+            // this.searchName();
+          }}
+        />
+        {users
+          .filter((u) => {
+            return u.name.toLowerCase().includes(searchData.toLowerCase());
+          })
+          .map((item) => {
+            return <h2 key={item.id}>{item.name}</h2>;
+          })}
       </div>
     );
   }
